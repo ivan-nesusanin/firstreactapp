a264fd625c4ec3aebadad352de436090a6a3f16c
ivan-nesusanin <l75x5@yandex.ru>
2022-08-15 15:08:02 +0300

    refactor: add mixins in navbar and footer; change favicon; add logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom';
+import { Image } from 'antd';
 import '../styles/NavBar.scss';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <Link to='/' className='link'>На главную</Link>
-      <Link to='/users' className='link'>Пользователи</Link>
+    <div className="navbar">
+      <Image
+        width={35}
+        src="https://cdn.icon-icons.com/icons2/1812/PNG/512/4213441-community-group-leader-people-team-users_115403.png"
+      />
+      <div>
+        <Link to="/" className="link">
+          На главную
+        </Link>
+        <Link to="/users" className="link">
+          Пользователи
+        </Link>
+      </div>
     </div>
   );
 };
